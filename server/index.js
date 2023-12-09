5585660be945bac20255306aebad976033b3b324
hazelkimberly <hazelkimc@gmail.com>
2023-12-08 18:37:22 -0800

    Define API routes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const reviewRouter = require('./reviewRoutes');
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan('dev'));
+
+// Set up routes
+// Use reviewRoutes.js file to handle endpoints that start with /reviews
+app.use('/reviews', reviewRouter);
 
 const PORT = process.env.PORT || 3000;
 
---
+++ server/reviewRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+// Creates a new router object
+const reviewRouter = express.Router();
+const {
+  getReviews,
+  getMeta,
+  addReview,
+  updateHelpfulCount,
+  reportReview,
+} = require('./reviewControllers');
+
+// Get list of reviews
+reviewRouter.get('/', getReviews);
+
+// Get metadata for a given product
+reviewRouter.get('/meta', getMeta);
+
+// Add a review
+reviewRouter.post('/', addReview);
+
+// Mark review as helpful
+reviewRouter.put('/:review_id/helpful', updateHelpfulCount);
+
+// Report review
+reviewRouter.put('/:review_id/report', reportReview);
+
+module.exports = reviewRouter;
